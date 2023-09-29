d2b2c1690800f083a609ec5f3b8ae3712f87eafc
solaa1 <145714061+solaa1@users.noreply.github.com>
2023-09-29 21:10:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let golden = document.querySelector('.golden')
+ function phone(x){
+    golden.src = x;
+}
+let clas = document.querySelector('.imgs')
+function hide(){
+    clas.classList.toggle('unhidden')
+}
